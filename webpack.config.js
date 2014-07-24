67f8fcfdd8ff24ac521dee76458fb5add75cd181
Dan Abramov <dan.abramov@gmail.com>
2014-07-24 20:02:28 +0300

    Put dev server in entry for consistency

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/dev-server',
     './scripts/example'
   ],
