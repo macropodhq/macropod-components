5f94c69788a07b885a0545c0d0330dc079c79214
deoxxa <deoxxa@fknsrs.biz>
2014-09-05 11:52:24 +1000

    remove mock api left over from example

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
+#!/usr/bin/env node
+
+var webpack = require('webpack'),
+    WebpackDevServer = require('webpack-dev-server');
+
 var config = require('./webpack.config');
-var cors = require('cors');
 
-var comments = [{author: 'Pete Hunt', text: 'Hey there!'},
-  {author: 'Justin Gordon', text: 'Aloha from @railsonmaui'}
-];
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-
-app.get('/comments.json', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
-});
-
-app.post('/comments.json', function(req, res) {
-  comments.push(req.body);
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
-});
-
-app.listen(3001);
-
-new WebpackDevServer(webpack(config), {
+var server = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true
-}).listen(3000, 'localhost', function (err, result) {
+});
+
+server.listen(3000, function(err) {
   if (err) {
-    console.log(err);
+    return console.warn(err);
   }
 
-  console.log('Listening at localhost:3000');
+  console.log('Listening: %j', this.address());
 });
