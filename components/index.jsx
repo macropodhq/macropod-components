a1af190eab16181acfae3a6155736511e100a3bc
deoxxa <deoxxa@fknsrs.biz>
2014-09-05 16:35:42 +1000

    make module instantiation manual, add styles

--- components/index.jsx
+++ components/index.jsx
@@ -1,15 +1,40 @@
 /** @jsx React.DOM */
 
-var react = require('react');
+var React = require('react');
+
+require("./index.scss");
+
+var Playground = React.createClass({
+  render: function() {
+    return (
+      <div className="playground">
+      	<h2>{this.props.name}</h2>
+      	<div>{this.props.children}</div>
+    	</div>
+    );
+  },
+});
+
+var getContainer = function getContainer(name) {
+	var id = ["container", name].join("-");
+
+	var el = document.getElementById(id);
+
+	if (!el) {
+		el = document.createElement("div");
+		el.id = id;
+		document.body.appendChild(el);
+	}
+
+	return el;
+};
+
+var render = function render(name, content) {
+	React.renderComponent(<Playground name={name}>{content}</Playground>, getContainer(name.replace(/\s/g, "-")));
+};
+
+// START DOING STUFF HERE
 
 var CommentBox = require('./comment-box');
 
-var modules = [
-	CommentBox,
-];
-
-modules.forEach(function(m) {
-	var el = document.createElement('div');
-	document.body.appendChild(el);
-	react.renderComponent(new m(), el);
-});
+render("Comment Box", <CommentBox data={[]}/>)
