b94219bbe51c1df704880efbad0a9199d405ac72
Geoff Stokes <geoff.stokes@bugherd.com>
2014-10-08 11:37:21 +1100

    Add loading component

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
         loaders: [
           'style',
           'css',
+          'autoprefixer',
           'sass?includePaths[]=./packages/base/style',
         ],
       },
