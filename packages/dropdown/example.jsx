c5b27e2d207b2ee5272b200da4d1555d967053b6
Geoff Stokes <geoff.stokes@bugherd.com>
2014-10-15 11:30:08 +1100

    Add Dropdown component
    Conflicts:
    	packages/index.jsx

---
+++ packages/dropdown/example.jsx
@@ -0,0 +1,42 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var Dropdown = require('./');
+
+var options = [
+  {
+    id: 1,
+    name: 'Google'
+  },
+  {
+    id: 2,
+    name: 'Yahoo'
+  },
+  {
+    id: 3,
+    name: 'Bing'
+  },
+  {
+    id: 4,
+    name: 'DuckDuckGo'
+  }
+];
+
+var DropdownExample = module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      selection: 1
+    };
+  },
+
+  handleChange: function(data) {
+    this.setState({selection: data.id});
+  },
+
+  render: function() {
+    return (
+      <Dropdown data={options} value={this.state.selection} onChange={this.handleChange} textField="name" valueField="id" duration={0} />
+    );
+  }
+});
