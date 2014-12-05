5111c3fcd9acf6f9bfe2db9268160eb6dcf56bbf
Matt Milosavljevic <matt@bugherd.com>
2014-12-05 13:30:45 +1000

    updating example

--- packages/app-header/example.jsx
+++ packages/app-header/example.jsx
@@ -7,11 +7,16 @@ var AppHeader = require('./');
 var Example = React.createClass({
   render: function() {
     return (
-      <AppHeader title="App Header">
-        <a href="#" className="AppHeader-brand-navigation-link">Home</a>
-        <a href="#" className="AppHeader-brand-navigation-link">About</a>
-        <a href="#" className="AppHeader-brand-navigation-link">Contact</a>
-      </AppHeader>
+      <AppHeader 
+        title="App Header"
+        navRight={
+          <span>
+            <a href="#" className="AppHeader-link">Home</a>
+            <a href="#" className="AppHeader-link">About</a>
+            <a href="#" className="AppHeader-link">Contact</a>
+          </span>
+        }
+      ></AppHeader>
     );
   }
 });
