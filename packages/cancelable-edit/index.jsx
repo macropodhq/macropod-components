a74998fa1d4ceda5dbc574fddba1d88d6e6ff7e4
Nathan Hutchison <nathan@bugherd.com>
2015-02-19 14:15:53 +1100

    more configuration for cancelable-edit

--- packages/cancelable-edit/index.jsx
+++ packages/cancelable-edit/index.jsx
@@ -27,12 +27,27 @@ module.exports = React.createClass({
 
   mixins: [KeyMixin],
   propTypes: {
+    value: React.PropTypes.string,
+    saveButtonText: React.PropTypes.string,
+    cancelButtonText: React.PropTypes.string,
+    saveButtonTitle: React.PropTypes.string,
+    saveButtonInvalid: React.PropTypes.string,
+    cancelButtonTitle: React.PropTypes.string,
     warnMessage: React.PropTypes.string,
-    displayName: React.PropTypes.string,
     small: React.PropTypes.bool,
+    inline: React.PropTypes.bool,
+    em: React.PropTypes.bool,
     allowEmpty: React.PropTypes.bool,
     autoSize: React.PropTypes.bool,
+    creating: React.PropTypes.bool,
     onSave: React.PropTypes.func.isRequired,
+    onCancel: React.PropTypes.func,
+    displayName(props, propName) {
+      if (propName in props) {
+        // displayName is deprecated
+        return new Error('CancelableEdit\'s type[' + props[propName] +'] property is deprecated. Please use saveButtonText.');
+      }
+    },
   },
 
   getInitialState() {
@@ -45,12 +60,22 @@ module.exports = React.createClass({
 
   getDefaultProps() {
     return {
+      value: '',
+      saveButtonText: 'Save',
+      saveButtonTitle: 'Save',
+      saveButtonTitleInvalid: 'Save',
+      cancelButtonText: 'Cancel',
+      cancelButtonTitle: 'Cancel',
       warnMessage: 'Are you sure you want to discard your changes?',
       displayName: '',
       small: false,
+      inline: false,
+      em: false,
       allowEmpty: false,
       autoSize: false,
+      creating: false,
       onSave: () => {},
+      onCancel: () => {},
     };
   },
 
@@ -61,7 +86,14 @@ module.exports = React.createClass({
   },
 
   handleConfirmCancel() {
-    this.setState({editing: false, showAlert: false});
+    this.setState({showAlert: false}, () => this.refs.input.getDOMNode().focus());
+  },
+
+  handleConfirmOk() {
+    this.setState({
+      editing: false,
+      showAlert: false,
+    }, () => this.props.onCancel());
   },
 
   handleCancel() {
@@ -73,17 +105,42 @@ module.exports = React.createClass({
   },
 
   handleSave() {
-    if (this.props.allowEmpty || (this.state.pendingValue !== '')) {
+    if (this.validInput()) {
+      const saveValue = this.state.pendingValue;
+
+      this.props.onSave(saveValue);
       this.setState({
         editing: false,
-      },
-        () => this.props.onSave(this.state.pendingValue)
-      );
+        pendingValue: '',
+      });
     }
   },
 
   unsaved() {
     return this.state.editing && (this.props.value !== this.state.pendingValue);
+  },
+
+  validInput() {
+    return this.props.allowEmpty ||
+      (typeof this.state.pendingValue === 'string' &&
+      (this.state.pendingValue !== ''));
+  },
+
+  focus() {
+    if (this.isMounted()) {
+      this.setState({
+        editing: true,
+        pendingValue: this.props.value,
+      }, () => this.refs.input.getDOMNode().focus());
+    }
+  },
+
+  handleBlur() {
+    if ((this.state.pendingValue.length < 1) || !this.unsaved()) {
+      this.setState({
+        editing: false
+      });
+    }
   },
 
   handleClick() {
@@ -95,36 +152,74 @@ module.exports = React.createClass({
     }
   },
 
-  render() {
-    const value = this.state.editing ? this.state.pendingValue : this.props.value;
-    const Textarea = this.props.autoSize ? AutoSizeTextArea : 'textarea';
-    
-    return (
-      <div>
-        <label style={{'display': 'none'}}>{this.props.name}</label>
+  renderContent() {
+    const editClassName = React.addons.classSet({
+      'CancelableEdit-edit': true,
+      'is-editing': this.state.editing,
+      'CancelableEdit-edit--em': this.props.em,
+    });
+
+    if (this.props.creating && !this.state.editing) {
+      return (
+        <Button skeleton style={{fontSize: 15, color: '#6aa0c3', pointer: 'cursor'}} onClick={this.handleClick}>{this.props.placeholder}</Button>
+      );
+    } else {
+      const value = this.state.editing ? this.state.pendingValue : this.props.value;
+      const Textarea = this.props.autoSize ? AutoSizeTextArea : 'textarea';
+
+      return (
+        [<label style={{'display': 'none'}}>{this.props.name}</label>,
         <Textarea
+          ref="input"
+          rows={this.props.inline ? 1 : 0}
           onKeyDown={this.keyHandler(hotKeys)}
-          className="CancelableEdit"
+          className={editClassName}
           value={value}
           onClick={this.handleClick}
           onChange={this.handleChange}
+          onBlur={this.handleBlur}
           name={this.props.name}
           readOnly={!this.state.editing}
           placeholder={this.props.placeholder}
-        />
+        />]
+      );
+    }
+  },
+
+  render() {
+    const className = React.addons.classSet({
+      [this.props.className]: !!this.props.className,
+      'CancelableEdit': true,
+      'CancelableEdit--inline': this.props.inline,
+    });
+
+    return (
+      <div className={className}>
+        { this.renderContent() }
         { this.state.editing &&
           <div className="CancelableEdit-control">
-            <Button small={this.props.small} skeleton onClick={this.handleCancel}>
-              Cancel
+            <Button
+                disabled={!this.validInput()}
+                title={this.validInput() ? this.props.saveButtonTitle : this.props.saveButtonTitleInvalid}
+                small={this.props.small}
+                success
+                onClick={this.handleSave}
+              >
+              {this.props.saveButtonText || 'Save ' + this.props.displayName}
             </Button>
-            <Button small={this.props.small} success onClick={this.handleSave}>
-              Save {this.props.displayName}
+            <Button
+                title={this.props.cancelButtonTitle}
+                small={this.props.small}
+                skeleton
+                onClick={this.handleCancel}
+              >
+              {this.props.cancelButtonText}
             </Button>
-            {this.state.showAlert &&
+            { this.state.showAlert &&
               <Alert
                   cancelable
-                  onOk={this.handleConfirmCancel}
-                  onCancel={this.setState.bind(this, {showAlert: false})}
+                  onOk={this.handleConfirmOk}
+                  onCancel={this.handleConfirmCancel}
                 >
                 {this.props.warnMessage}
               </Alert>
