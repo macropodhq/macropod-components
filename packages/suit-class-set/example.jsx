0739ca0290c1486fa02d0c391b378f15c1b6bf06
Jessica Stokes <jessica.stokes@macropod.com>
2015-02-25 11:12:14 +1100

    Add SuitClassSet, a replacement for classSet
    Intended to be more strict than classSet itself, and restrict implementors to using the [SUIT CSS naming conventions](https://github.com/suitcss/suit/blob/master/doc/naming-conventions.md).

---
+++ packages/suit-class-set/example.jsx
@@ -0,0 +1,45 @@
+'use strict';
+
+const React = require('react');
+
+const SuitClassSet = require('./');
+
+module.exports = React.createClass({
+  displayName: 'SuitClassSet-example',
+
+  render() {
+    const aClassSet = new SuitClassSet('SuitClassSet');
+    const aDescendentClassSet = aClassSet.createDescendent('descendent');
+
+    aClassSet.addModifier('foo');
+
+    aClassSet.addModifier({
+      'bar': true,
+      'baz': false,
+      'something with spaces which should not work': true,
+    });
+
+    aClassSet.addModifier([
+      'abc',
+      '123',
+    ]);
+
+    aClassSet.addState('loading');
+
+    aDescendentClassSet.addModifier('withModifier');
+
+    return (
+      <div>
+        <p>This example contains two SuitClassSets; one demonstrating all the modifier and state features, and one which is a descendent whose class name is derived from the first.</p>
+        <h3><code>aClassSet</code></h3>
+        <pre>
+          {aClassSet.toString()}
+        </pre>
+        <h3><code>aDescendentClassSet</code></h3>
+        <pre>
+          {aDescendentClassSet.toString()}
+        </pre>
+      </div>
+    );
+  }
+});
---
+++ packages/suit-class-set/index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const _ = require('lodash-node');
+const empty = '';
+const space = ' ';
+const dash = '-';
+const doubleDash = dash+dash;
+
+function SuitClassSet(componentName) {
+  if (!isValidString(componentName)) {
+    throw new Error(`Supplied primary class name "${componentName}" is invalid.`);
+  }
+
+  Object.defineProperties(this, {
+
+    componentName: {
+      configurable: true,
+      value: componentName,
+    },
+
+    modifiers: {
+      value: [],
+      writable: true
+    },
+
+    states: {
+      value: [],
+      writable: true
+    }
+
+  });
+}
+
+function isValidString(string) {
+  return _.isString(string) && string.indexOf(space) === -1;
+}
+
+function processString(string) {
+  if (isValidString(string) && string.indexOf(dash) === -1) {
+
+    return string;
+
+  } else {
+
+    console.warn(`Supplied class name fragments must not contain spaces. Ignoring "${string}"`);
+    return empty;
+
+  }
+}
+
+Object.defineProperty(SuitClassSet.prototype, 'add', {
+  value: function(collection, args) {
+    Array.prototype.forEach.call(args, arg => {
+
+      if (_.isString(arg)) {
+        this[collection].push(processString(arg));
+      }
+
+      if (_.isArray(arg)) {
+        this[collection] = this[collection].concat(_.map(arg, item => processString(item)));
+      }
+
+      if (_.isPlainObject(arg)) {
+        this[collection] = this[collection].concat(_(arg).map((item, key) => {
+          if (item) {
+            return processString(key);
+          } else {
+            return empty;
+          }
+        }).value());
+      }
+
+    });
+  }
+});
+
+SuitClassSet.prototype.addModifier = function() {
+  this.add('modifiers', arguments);
+};
+
+SuitClassSet.prototype.addState = function() {
+  this.add('states', arguments);
+};
+
+SuitClassSet.prototype.createDescendent = function(descendentName) {
+  if (!isValidString(descendentName)) {
+    throw new Error(`Supplied descendent name "${descendentName}" is invalid.`);
+  }
+
+  return new SuitClassSet([this.componentName, descendentName].join(dash));
+};
+
+SuitClassSet.prototype.toArray = function() {
+  return [this.componentName]
+    .concat(
+      _(this.modifiers)
+        .uniq()
+        .filter(subClass => subClass.length > 0)
+        .map(subClass => [this.componentName, subClass].join(doubleDash))
+        .value()
+    )
+    .concat(
+      _(this.states)
+        .uniq()
+        .filter(state => state.length > 0)
+        .map(state => `is-${state}`)
+        .value()
+    );
+};
+
+SuitClassSet.prototype.toString = function() {
+  return this.toArray().join(space);
+};
+
+module.exports = SuitClassSet;
