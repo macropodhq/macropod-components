fbc8a40cc33631c47899b2cc9956364995e72ef9
James Coleman <james.coleman@bugherd.com>
2015-03-11 10:43:46 +1100

    Include creating tags in example

--- packages/tag-input/example.jsx
+++ packages/tag-input/example.jsx
@@ -1,50 +1,49 @@
 'use strict';
 
 const React = require('react');
-
+const _ = require('lodash-node');
 const TagInput = require('./');
-
-const options = [
-  {
-    id: 1,
-    name: 'Eggs'
-  },
-  {
-    id: 2,
-    name: 'Bacon'
-  },
-  {
-    id: 3,
-    name: 'Hash Browns'
-  },
-  {
-    id: 4,
-    name: 'Sausage'
-  },
-  {
-    id: 5,
-    name: 'Tomato'
-  },
-  {
-    id: 6,
-    name: 'Toast'
-  },
-  {
-    id: 7,
-    name: 'Quinoa'
-  },
-  {
-    id: 8,
-    name: 'Salmon'
-  }
-];
 
 module.exports = React.createClass({
   displayName: 'TagInputExample',
 
   getInitialState() {
     return {
-      value: null
+      value: [],
+      options: [
+        {
+          id: 1,
+          name: 'Eggs'
+        },
+        {
+          id: 2,
+          name: 'Bacon'
+        },
+        {
+          id: 3,
+          name: 'Hash Browns'
+        },
+        {
+          id: 4,
+          name: 'Sausage'
+        },
+        {
+          id: 5,
+          name: 'Tomato'
+        },
+        {
+          id: 6,
+          name: 'Toast'
+        },
+        {
+          id: 7,
+          name: 'Quinoa'
+        },
+        {
+          id: 8,
+          name: 'Salmon'
+        }
+      ]
     };
   },
 
@@ -54,9 +53,36 @@ module.exports = React.createClass({
     });
   },
 
+  handleCreate(value) {
+    if (typeof value === 'string' && value.length > 0) {
+      const newValue = {
+        id: this.state.options.length + 2,
+        name: value
+      };
+
+      var options = this.state.options.slice(0);
+      options.push(newValue);
+
+      this.setState({options: options});
+
+      var value = this.state.value.slice(0);
+      value.push(newValue);
+
+      this.handleChange(value);
+    }
+  },
+
   render() {
     return (
-      <TagInput onChange={this.handleChange} data={options} placeholder="Make some breakfast..." duration={0} textField="name" valueField="id" value={this.state.value}/>
+      <TagInput
+        onChange={this.handleChange}
+        data={this.state.options}
+        placeholder="Make some breakfast..."
+        duration={0}
+        textField="name"
+        valueField="id"
+        value={this.state.value}
+        onCreate={this.handleCreate} />
     );
   }
 });
