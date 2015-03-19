c8646fc23a284154285a721801557b4b0c8de37b
James Coleman <james.coleman@bugherd.com>
2015-03-20 10:14:06 +1100

    Put padding on the BarItem child not the wrapper itself

--- packages/bar-item/index.jsx
+++ packages/bar-item/index.jsx
@@ -5,15 +5,9 @@ const React = require('react');
 module.exports = React.createClass({
   displayName: 'BarItem',
 
-  getDefaultProps() {
-    return {
-      notLink: false
-    };
-  },
-
   render() {
-    var style = {
-      padding: 15,
+    const style = {
+      padding: typeof this.props.children === 'string' ? 15 : 0,
       display: 'inline-block',
       color: 'rgba(255,255,255,0.5)',
       borderLeft: this.props.right ? '1px solid rgba(255, 255, 255, 0.2)' : 0,
@@ -22,7 +16,26 @@ module.exports = React.createClass({
 
     return (
       <span className="BarItem" style={style}>
-        {this.props.children}
+        {
+          React.Children.map(this.props.children, (element) => {
+            if (typeof element === 'string') {
+              return element;
+            }
+
+            var style = {};
+
+            if (element.props && element.props.style) {
+              style = element.props.style;
+            }
+
+            style = Object.assign(style, {
+              padding: 15,
+              display: 'inline-block',
+            });
+
+            return React.addons.cloneWithProps(element, {style: style});
+          })
+        }
       </span>
     );
   }
