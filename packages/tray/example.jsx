678e788651aeb450a21994eb0b8f8254f0d18f24
James Coleman <james.coleman@bugherd.com>
2015-03-30 12:21:26 +1100

    Tray

---
+++ packages/tray/example.jsx
@@ -0,0 +1,153 @@
+'use strict';
+
+const React = require('react');
+
+const Tray = require('./');
+const Button = require('../button');
+const Bar = require('../bar');
+const Icon = require('../icon');
+const Link = require('../link');
+
+const style = {
+  bubble: {
+    background: 'rgba(199, 201, 209, 0.4)',
+    borderRadius: 5,
+    padding: '10px 10px',
+    margin: '0 0 10px 0',
+  },
+
+  bubbleAlt: {
+    background: 'rgba(255, 255, 255, 1)',
+    borderRadius: 5,
+    padding: '10px 10px',
+    margin: '0 0 10px 0',
+  },
+
+  link: {
+    fontSize: 13,
+    display: 'block',
+    color: '#3e465c',
+  },
+};
+
+module.exports = React.createClass({
+  displayName: 'TrayExample',
+
+  statics: {
+    wide: true,
+  },
+
+  getInitialState() {
+    return {
+      show: true,
+      stickyHeight: 0,
+    };
+  },
+
+  handleToggle() {
+    this.setState({
+      show: !this.state.show,
+    });
+  },
+
+  handleStickyHeight(height) {
+    this.setState({
+      stickyHeight: height,
+    });
+  },
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.handleToggle}>{this.state.show ? 'Hide' : 'Show'}</Button>
+
+        { this.state.show &&
+            <div style={{
+              position: 'relative',
+              height: 400,
+              overflow: 'hidden',
+              marginTop: 40,
+            }}>
+              <Tray>
+                <Tray.Group>
+                  <Tray.Item>
+                    <Link style={style.link}>Dashboard</Link>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <Link style={style.link}>Task List</Link>
+                  </Tray.Item>
+                </Tray.Group>
+
+                <Tray.Group title="Projects">
+                  <Tray.Item>
+                    <Link style={style.link}>Design-o-rama</Link>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <Link style={style.link}>Macropod ID design</Link>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <Link style={style.link}>Macropod Website</Link>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <Link style={style.link}>Marketing and Comms</Link>
+                  </Tray.Item>
+                </Tray.Group>
+
+                <Tray.Group title="Custom item rendering">
+                  <Tray.Item>
+                    <Link style={{
+                      background: 'rgb(245, 247, 249)',
+                      padding: 5,
+                      textAlign: 'center',
+                      borderRadius: 3,
+                      display: 'inline-block',
+                      width: '100%',
+                    }}>
+                      There are no items here! Click to add
+                    </Link>
+                  </Tray.Item>
+                </Tray.Group>
+              </Tray>
+              <Tray align={Tray.align.RIGHT} stickyHeight={this.state.stickyHeight}>
+                <Tray.Sticky onCalculateHeight={this.handleStickyHeight}>
+                  <Bar>
+                    <Bar.Item style={{border: 0}}>Comment Feed</Bar.Item>
+                    <Bar.Item align={Bar.Item.align.RIGHT}><Icon type="refresh" /></Bar.Item>
+                  </Bar>
+                </Tray.Sticky>
+
+                <Tray.Group>
+                  <Tray.Item>
+                    <div style={style.bubble}>
+                      <p>Is he a bad guy?</p>
+                    </div>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <div style={style.bubbleAlt}>
+                      <p>Yeah.</p>
+                    </div>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <div style={style.bubble}>
+                      <p>How can you tell?</p>
+                    </div>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <div style={style.bubbleAlt}>
+                      <p>Because he's a shark.</p>
+                    </div>
+                  </Tray.Item>
+                  <Tray.Item>
+                    <div style={style.bubble}>
+                      <p>There's no good sharks?</p>
+                    </div>
+                  </Tray.Item>
+                </Tray.Group>
+              </Tray>
+            </div>
+          }
+      </div>
+    );
+  }
+});
+
---
+++ packages/tray/index.jsx
@@ -0,0 +1,196 @@
+'use strict';
+
+const React = require('react');
+const _ = require('lodash-node');
+
+require('./tray.scss');
+
+const align = {
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+};
+
+const style = {
+  title: {
+    textTransform: 'uppercase',
+    color: 'rgb(199, 201, 209)',
+    fontSize: 10,
+    letterSpacing: 0.5,
+    margin: '0 0 4px 0',
+  },
+
+  group: {
+    margin: 0,
+    padding: '15px 0',
+    borderBottom: '1px solid rgba(238, 238, 238, 1)',
+  },
+
+  tray: {
+    boxShadow: '0 1px 5px rgba(0, 0, 0, 0.4)',
+    background: 'rgb(251, 251, 254)',
+    width: 300,
+    maxWidth: '90%',
+    top: 0,
+    bottom: 0,
+  },
+
+  content: {
+    padding: 10,
+    overflow: 'auto',
+  },
+
+  item: {
+    margin: 0,
+    padding: '3px 0',
+  }
+};
+
+const Group = React.createClass({
+  displayName: 'TrayGroup',
+
+  propTypes: {
+    title: React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      style: {},
+    };
+  },
+
+  getStyle() {
+    return Object.assign({}, style.group, this.props.style);
+  },
+
+  render() {
+    var title = null;
+
+    if (this.props.title) {
+      title = (
+        <dt style={style.title}>
+          {this.props.title}
+        </dt>
+      );
+    }
+
+    return (
+      <dl className="Tray-Group" style={this.getStyle()}>
+        {title}
+        {this.props.children}
+      </dl>
+    )
+  }
+});
+
+const Item = React.createClass({
+  displayName: 'TrayItem',
+
+  getDefaultProps() {
+    return {
+      style: {},
+    };
+  },
+
+  getStyle() {
+    return Object.assign({}, style.item, this.props.style);
+  },
+
+  render() {
+    return (
+      <dd className="Tray-Item" style={style.item}>
+        {this.props.children}
+      </dd>
+    )
+  }
+});
+
+const Sticky = React.createClass({
+  displayName: 'TraySticky',
+
+  propTypes: {
+    onCalculateHeight: React.PropTypes.func.isRequired,
+  },
+
+  componentDidMount() {
+    this.props.onCalculateHeight(this.getDOMNode().getBoundingClientRect().height);
+  },
+
+  render() {
+    return (
+      <div className="Tray-Sticky">
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+module.exports = React.createClass({
+  displayName: 'Tray',
+
+  statics: {
+    align: align,
+    Group: Group,
+    Item: Item,
+    Sticky: Sticky,
+  },
+
+  propTypes: {
+    align: React.PropTypes.oneOf(Object.keys(align)),
+    stickyHeight: React.PropTypes.number,
+    fixed: React.PropTypes.bool,
+  },
+
+  getDefaultProps() {
+    return {
+      align: align.LEFT,
+      fixed: false,
+      stickyHeight: 0,
+      style: {},
+    };
+  },
+
+  buildSticky() {
+    return _.filter(this.props.children, (element) => {
+      if (element.type.displayName === Sticky.displayName) {
+        return element;
+      }
+
+      return false;
+    });
+  },
+
+  buildContent() {
+    return _.filter(this.props.children, (element) => {
+      if (element.type.displayName !== Sticky.displayName) {
+        return element;
+      }
+
+      return false;
+    });
+  },
+
+  getTrayStyle() {
+    return Object.assign({}, style.tray, {
+      position: this.props.fixed ? 'fixed' : 'absolute',
+      left: this.props.align === align.RIGHT ? 'auto' : 0,
+      right: this.props.align === align.RIGHT ? 0 : 'auto',
+    }, this.props.style);
+  },
+
+  getContentStyle() {
+    return Object.assign({}, style.content, {
+      height: 'calc(100% - ' + this.props.stickyHeight + 'px)',
+    });
+  },
+
+  render() {
+    return (
+      <section className="Tray" style={this.getTrayStyle()}>
+        {this.buildSticky()}
+        <div className="Tray-Content" style={this.getContentStyle()}>
+          {this.buildContent()}
+        </div>
+      </section>
+    );
+  }
+});
