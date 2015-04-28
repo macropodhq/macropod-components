183dfadbd535c08e2e41d581a3b621c9450d9a54
Matt Milosavljevic <matt@bugherd.com>
2015-04-28 14:11:05 +1000

    move link group styles to sass

--- packages/link-group/index.jsx
+++ packages/link-group/index.jsx
@@ -6,38 +6,6 @@ const React = require('react');
 const Router = require('react-router');
 const _ = require('lodash-node');
 
-const border = '2px solid black';
-const style = {
-  link: {
-    general: {
-      padding: '1em',
-      background: 'grey',
-      border: '1px solid black',
-      borderTop: border,
-      borderBottom: border,
-      float: 'left',
-    },
-
-    first: {
-      borderRadius: '3px 0 0 3px',
-      borderLeft: border,
-    },
-
-    last: {
-      borderRadius: '0 3px 3px 0',
-      borderRight: border,
-    },
-
-    active: {
-      background: 'blue',
-    },
-  },
-
-  clear: {
-    clear: 'both',
-  }
-};
-
 module.exports = React.createClass({
   displayName: 'LinkGroup',
 
@@ -53,21 +21,14 @@ module.exports = React.createClass({
     let children = [];
 
     React.Children.forEach(this.props.children, (child, index) => {
-      let childStyle = style.link.general;
+      let className = child.props.className ? child.props.className : '';
 
-      if (index === 0) {
-        childStyle = Object.assign({}, childStyle, style.link.first);
+
+      if (child.props && (!_.size(this.getQuery()) && child.props.default)) {
+        className = className + ' active';
       }
 
-      if (index === ((this.props.children).length - 1)) {
-        childStyle = Object.assign({}, childStyle, style.link.last);
-      }
-
-      if (child.props && (!_.size(this.getQuery()) && child.props.default) || (_.isEqual(Object.keys(child.props.query), Object.keys(this.getQuery())))) {
-        childStyle = Object.assign({}, childStyle, style.link.active);
-      }
-
-      children.push(React.addons.cloneWithProps(child, {style: childStyle}));
+      children.push(React.addons.cloneWithProps(child, {className: className}));
     });
 
     return children;
@@ -77,7 +38,6 @@ module.exports = React.createClass({
     return (
       <div className="LinkGroup" style={this.props.style}>
         {this.getChildren()}
-        <table style={style.clear} />
       </div>
     );
   }
