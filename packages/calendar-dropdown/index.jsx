c60d8894a22db03e97eaf8071475126e96d47a2c
James Coleman <james.coleman@bugherd.com>
2015-05-28 10:15:00 +1000

    postcss

--- packages/calendar-dropdown/index.jsx
+++ packages/calendar-dropdown/index.jsx
@@ -3,7 +3,7 @@
 const React = require('react/addons');
 const Widgets = require('react-widgets');
 
-require('./calendar-dropdown.scss');
+// require('./calendar-dropdown.scss');
 
 // TODO: handle null date
 module.exports = React.createClass({
