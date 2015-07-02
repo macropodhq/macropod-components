3a3109225b6c124e718c048a4def02c94b145392
Jessica Stokes <jessica.stokes@macropod.com>
2015-07-02 14:26:24 +1000

    Adds `MarkdownSnippet`
    `MarkdownSnippet` is a component for rendering formatted Markdown. Hopefully one day it'll be used inside MarkdownEdit, but for now it's its own thing.

---
+++ packages/markdown-snippet/example.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import MarkdownSnippet from './';
+
+const markdown = `##### Markdown Snippet
+
+> Here's a blockquote!!!
+
+This is a Markdown snippet. You can do _all_ kinds of strange things in [Markdown](http://daringfireball.net)!!!`;
+
+class MarkdownSnippetExample extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Standard</h4>
+        <MarkdownSnippet
+          markdown={markdown}
+        />
+
+        <h4>Without default styles</h4>
+        <MarkdownSnippet
+          defaultStyles={false}
+          markdown={markdown}
+        />
+
+        <h4>Change link target</h4>
+        <MarkdownSnippet
+          markdown={markdown}
+          linkTarget="_blank"
+        />
+      </div>
+    );
+  }
+}
+
+export default MarkdownSnippetExample;
