2618b515497fa248739014ab7bf2ace03a6d650f
Rasta <rastafario@gmail.com>
2015-08-05 23:21:30 -0500

    Merged the arrow option for dropdown and inputtag components

--- packages/dropdown/index.jsx
+++ packages/dropdown/index.jsx
@@ -9,7 +9,61 @@ require('./dropdown.scss');
 module.exports = React.createClass({
   displayName: 'Dropdown',
 
+  getDefaultProps() {
+    return {
+      label: '',
+      arrow: false,
+      overlayShown: false,
+    };
+  },
+
+  getInitialState() {
+    return {
+      open: false,
+    }
+  },
+
+  changeHandler() {
+  	this.setState({
+      open: !this.state.open,
+      overlayShown: !this.state.open,
+    });
+  },
+
+  inputWrapper() {
+    return (
+    	<Widgets.DropdownList {...this.props} />
+    );
+  },
+
+  arrowWrapper() {
+    let layer = null;
+
+    if (this.state.overlayShown) {
+      layer = (
+        <div onClick={this.changeHandler} className="overlay" />
+      );
+    }
+
+  	return (
+  		<div className="dropdown-arrow-container">
+  			<button onClick={this.changeHandler} className="button-trigger">{this.props.label}<div/></button>
+        {layer}
+        <Widgets.DropdownList
+  				onSelect={this.changeHandler}
+  				open={this.state.open}
+  				{...this.props}
+  			/>
+   		</div>
+   	);
+  },
+
   render() {
-    return (<Widgets.DropdownList {...this.props} />);
+
+    if (this.props.arrow) {
+      return this.arrowWrapper();
+    }
+
+    return this.inputWrapper();
   },
 });
--- packages/form/input-tag.jsx
+++ packages/form/input-tag.jsx
@@ -3,19 +3,55 @@
 const React = require('react');
 const InputWrapper = require('./input-wrapper');
 const Widgets = require('react-widgets');
+const cx = require('classnames');
 
 require('./input-tag.scss');
 
 module.exports = React.createClass({
+
   displayName: 'InputTag',
 
   getDefaultProps() {
     return {
       label: '',
+      arrow: false,
+      layerShown: false,
     };
   },
 
-  render() {
+  getInitialState() {
+    return {
+      open: false,
+    };
+  },
+
+  changeHandler() {
+    this.setState({
+      open: !this.state.open,
+      overlayShown: !this.state.open,
+    });
+  },
+
+  multiSelect(normalisedProps) {
+    if (this.props.arrow) {
+      return (
+        <Widgets.Multiselect
+          {...this.props}
+          open={this.state.open}
+          duration={50}
+        />
+      );
+    } else {
+      return (
+        <Widgets.Multiselect
+          {...normalisedProps}
+          duration={50}
+        />
+      );
+    }
+  },
+
+  inputWrapper() {
     const normalisedProps = InputWrapper.normaliseProps(this.props);
 
     return (
@@ -23,11 +59,39 @@ module.exports = React.createClass({
         {...normalisedProps}
         inputType="Tag"
       >
-        <Widgets.Multiselect
-          {...normalisedProps}
-          duration={50}
-        />
+        {this.multiSelect(normalisedProps)}
       </InputWrapper>
     );
+  },
+
+  arrowWrapper() {
+    let layer = null;
+
+    if (this.state.overlayShown) {
+      layer = (
+        <div onClick={this.changeHandler} className="overlay" />
+      );
+    }
+
+    let classes = cx({
+      'empty': this.state.isEmpty,
+      'multiselect-arrow-container':true
+    });
+
+    return (
+      <div className={classes}>
+        <button onClick={this.changeHandler} className="button-trigger">{this.props.label}<div/></button>
+        {layer}
+        {this.multiSelect()}
+      </div>
+    );
+  },
+
+  render() {
+    if (this.props.arrow) {
+      return this.arrowWrapper();
+    }
+
+    return this.inputWrapper();
   },
 });
