d5e68f1842d16a0fc69c3880fe74bc51b8e17068
Jessica Stokes <jessica.stokes@macropod.com>
2015-10-07 09:31:34 +1100

    Fix an issue with the hot reload
    `.js` files took priority over `.jsx` files and as such the examples would only ever load the compiled versions. Rearranging the priority here fixes this behaviour.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ var config = require('macropod-tools/webpack.config');
 
 config.module.loaders.push({test: /\.md$/, loaders: ['html-loader', 'markdown-loader']});
 
+// De-prioritise loading `.js` files so `.jsx` files take priority.
+config.resolve.extensions.splice(config.resolve.extensions.indexOf('.js'), 1);
+config.resolve.extensions.push('.js');
+
 if (typeof config.resolve.alias !== 'object') {
   config.resolve.alias = {};
 }
